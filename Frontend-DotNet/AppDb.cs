05cb78fea21ec758e917a5c24f4259a0c794caa2
Divyanshu Kumar <divyanshukumarg@gmail.com>
2020-05-21 12:49:06 +0530

    Databse integration done, finally :) :) yaya

---
+++ Frontend-DotNet/AppDb.cs
@@ -0,0 +1,17 @@
+using System;
+using MySql.Data.MySqlClient;
+
+namespace Frontend
+{
+    public class AppDb : IDisposable
+    {
+        public MySqlConnection Connection { get; }
+
+        public AppDb(string connectionString)
+        {
+            Connection = new MySqlConnection(connectionString);
+        }
+
+        public void Dispose() => Connection.Dispose();
+    }
+}
--- Frontend-DotNet/Startup.cs
+++ Frontend-DotNet/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Http;
+using MySql.Data.MySqlClient;
 
 namespace Frontend
 {
@@ -29,6 +30,8 @@ namespace Frontend
         {
             _dbUrl = Configuration["DbUrl"];
             _dbPassword = Configuration["DbPassword"];
+            services.AddTransient<AppDb>(_ => new AppDb(Configuration["ConnectionUrl"]));
+            services.AddTransient<MySqlConnection>(_ => new MySqlConnection(Configuration["ConnectionStrings:Default"]));
             services.AddRazorPages();
         }
 
@@ -64,7 +67,15 @@ namespace Frontend
 
               endpoints.MapGet("/hello/{name:alpha}", async context =>
               {
-                  var name = context.Request.RouteValues["name"];
+                  using AppDb Db = new AppDb(Configuration["ConnectionUrl"]);
+                  await Db.Connection.OpenAsync();
+                  string name = (string) context.Request.RouteValues["name"];
+                  string query = "Insert into test2 values('"+name+ "');";
+                  //console.log(query);
+                  using var cmd = new MySqlCommand(query, Db.Connection);
+                  //BindParams(cmd);
+                  await cmd.ExecuteNonQueryAsync();
+                  //Id = (int) cmd.LastInsertedId;
                   await context.Response.WriteAsync($"Hello {name}!");
               });
                 //endpoints.MapRazorPages();
